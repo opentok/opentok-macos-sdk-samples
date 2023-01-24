dd8014fe0968e62d15f1a20a9efc209ce5ce3573
Jeronimo Valli <jeronimo.valli@gmail.com>
2023-01-24 12:51:44 +0000

    OPENTOK-49993: Screen-Sharing app included
    * Screen-Sharing app included
    
    * include readme notes

---
+++ Screen-Sharing/Screen-Sharing/Screen-Sharing/Screen_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_SharingApp.swift
+//  Screen-Sharing
+//
+//  Created by Jerónimo Valli on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Screen_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
